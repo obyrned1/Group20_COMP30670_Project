3b662991c3577ad6ccf3f4d724bb82a833038220
oleathlc <33549380+oleathlc@users.noreply.github.com>
2018-03-12 17:05:30 +0000

    Removed unneccessary parts to the flask app

--- flaskapp/app/views.py
+++ flaskapp/app/views.py
@@ -2,12 +2,9 @@
 
 from flask import render_template
 from app import app
-from sysinfo import sysinfo
 
 @app.route('/')
 def index():
 	returnDict = {}
-	returnDict['user'] = 'clawlor90' 
-	returnDict['Title'] = 'Home'
-	returnDict['platform'] = sysinfo.main()
+	returnDict['Title'] = 'Dublin Bike Planner'
 	return render_template("index.html", **returnDict)
