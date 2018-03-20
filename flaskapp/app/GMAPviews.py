e7e57fe13dd4e00e59641d70655d1e5565ed461e
oleathlc <33549380+oleathlc@users.noreply.github.com>
2018-03-20 19:28:12 +0000

    Flask app changes

--- flaskapp/app/GMAPviews.py
+++ flaskapp/app/GMAPviews.py
@@ -1,11 +1,36 @@
 # dummmyapp/app/views.py
 
+from sqlalchemy import create_engine
 from flask import render_template
 from app import app
+from flask import jsonify
+
+'''
+def connectDB():
+    try:
+        # http://docs.sqlalchemy.org/en/latest/core/engines.html
+        engine = create_engine("mysql+mysqldb://ScrumMasterG20:Toxicbuzz18@dublin-bikes-data.csu7egshtvlv.us-west-2.rds.amazonaws.com:3306/DublinBikesData", echo = True)
+        return engine
+
+    except Exception as e:
+        print("Error:", type(e))
+        print(e)
+'''
 
 @app.route('/')
 def index():
         returnDict = {}
         returnDict['Title'] = 'Dublin Bike Planner'
         return render_template("index.html", **returnDict)
-
+    
+'''
+@app.route('/stations')
+def getStationData():
+        engine = connectDB()
+        conn = engine.connect()
+        stations =[]
+        rows = conn.execute("SELECT * FROM StaticData")
+        for row in rows:
+            stations.append(dict(row))
+        return jsonify(stations=stations)
+'''
