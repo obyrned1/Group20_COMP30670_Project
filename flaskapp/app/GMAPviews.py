25cc39ed1cfa9ab73ac17c7b609cecf45cc4579b
Emmet Tracey <emtracey@tcd.ie>
2018-03-30 11:15:44 +0100

    Infoboxes now work using API scrape data

--- flaskapp/app/GMAPviews.py
+++ flaskapp/app/GMAPviews.py
@@ -13,20 +13,6 @@ import csv
 import time
 
 
-
-#===============================================================================
-# def connectDB():
-#     try:
-#         # http://docs.sqlalchemy.org/en/latest/core/engines.html
-#         engine = create_engine("mysql+mysqldb://ScrumMasterG20:Toxicbuzz18@dublinbikes.cvzzy1efxyiq.us-east-2.rds.amazonaws.com:3306/DublinBikesData", echo = False)
-#         return engine
-#  
-#     except Exception as e:
-#         print("Error:", type(e))
-#         print(e)
-#===============================================================================
-  
-
 def connectDB():
     try:
         # http://docs.sqlalchemy.org/en/latest/core/engines.html
@@ -43,10 +29,12 @@ def index():
     returnDict = {}
     returnDict['Title'] = 'Dublin Bike Planner'
     returnDict['Stations'] = getDynamicData()
+    #returnDict['DynamicStations'] = getDynamicData()
     return render_template("index.html", **returnDict)
-      
+    
+# Will need to use the below to add robustness - if API goes down we will use this to place markers
 #===============================================================================
-# #@app.route('/stations')
+# @app.route('/stations')
 # def getStationData():
 #     engine = connectDB()
 #     conn = engine.connect()
@@ -65,17 +53,5 @@ def getDynamicData():
     # https://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file
     str_file = file.read().decode('utf-8')
     standData = json.loads(str_file)
-    #print(standData)
-    dydata = {}
-    for i in range(0,100,1):
-    #there are 100 stations as given by JCDecaux json
-        standNum = standData[i]['number']
-        standAddress = standData[i]['address']
-        standBikes = standData[i]['bike_stands']
-        standStatus = standData[i]['status']
-        standAvailableBikes = standData[i]['available_bikes']
-        standAvailableSpaces = standData[i]['available_bike_stands']
-        dateTime = (standData[i]['last_update'] / 1000 )
-        standLastUpdate = datetime.datetime.fromtimestamp(dateTime).strftime('%Y-%m-%d %H:%M:%S')
-        dydata[standNum] =  standAddress, standStatus, standBikes, standAvailableBikes, standAvailableSpaces,  standLastUpdate
-    return dydata
+    return standData
+
